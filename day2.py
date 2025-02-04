a412d5d15689518e51d8ed824c0f61f0dd545630
Sai shruthi Haridasu <138649324+Shruthi6641@users.noreply.github.com>
2025-02-05 01:02:15 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,102 @@
+#printing numbers
+print(68)
+#output:68
+print(54.6)
+#output:54.6
+
+
+
+
+#Arthimetic operations:+,-,*,/,%,**,//
+
+
+print(6+4)
+#output:10
+print(6-4)
+#output:2
+print(6*4)
+#output:24
+print(6**4)
+#output:1296
+print(6/4)
+#output:1.5
+print(6%4)
+#output:2
+print(6//4)
+#output:1
+
+#arthimetic operations using variables
+a=42
+b=30
+print(a+b,a-b,a*b,a**b,a/b,a%b,a//b)
+#output:(72,12,1260,4982860305982791130999719267476828621962135732224,1.4,12,1)
+
+#printing type of variable
+a=42
+print(type(a))
+#output:<class 'int'>
+b=30
+print(type(b))
+#output:<class 'int'>
+
+#printing strings
+x="hello world"
+print(x)
+#output:hello world
+
+
+
+#printing the length of string
+x="hello world"
+print(len(x))
+#output:11
+
+#printing type 
+x="hello world"
+print(type(x))
+#output:<class 'str'>
+
+
+#indexing
+x="hello world"
+print(x[3])
+#output:l
+print(x[8])
+#output:r
+
+
+#negative indexing
+print(x[-4])
+#output:o
+
+
+
+
+
+
+
+
+
+#slicing
+x="hello world"
+print(x[3:7])
+#output:lo w
+print(x[3:])
+#output:lo world
+print(x[:12])
+#output:hello world
+print(x[-4:-2])
+#output:or
+print(x[-2:-8:-1])
+#output:lrow o
+
+
+#step
+x="hello world"
+print(x[2:9:2])
+#output:lowr
+print(x[-3:-7:-2])
+#output:rw
+
+
+
